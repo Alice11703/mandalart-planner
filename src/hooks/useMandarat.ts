974a4b99b4777235fb5456150ffc630b45beae9a
Alice <hing9ugii@gmail.com>
2025-02-20 21:04:50 +0900

    fix build error

---
+++ src/hooks/useMandarat.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export const useMandarat = (initialBoard: string[]) => {
+  const [board, setBoard] = useState<string[]>(initialBoard);
+
+  const updateCell = (index: number, value: string) => {
+    const newBoard = [...board];
+    newBoard[index] = value;
+    setBoard(newBoard);
+  };
+
+  return {
+    board,
+    updateCell,
+  };
+}; 
